aa8fd1d7a107e6fa9a5ae61fe773c5098e22d991
Z <zkenaga@gmail.com>
2015-05-28 20:44:59 -0400

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy #when you delete post it also deletes comment
 	validates :title, presence:true, length: { minimum: 5 }
 	validates :body, presence:true
 end
